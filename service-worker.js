b58dd197015eee41ee0f97fc3b432f6104a5425e
iryanto95 <iryanto62@gmail.com>
2023-02-18 18:45:09 +0800

    updated root to v2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0222bdf9db63afbc813bf4166bbc8ea5.js"
+  "/precache-manifest.11dafa7dafafa602e58f0bb9011a47bc.js"
 );
 
 self.addEventListener('message', (event) => {
