0f5ef3d76bac0784b61158a133e867fe888a1d7a
iryanto95 <iryanto62@gmail.com>
2023-02-20 08:53:00 +0800

    rolled back package-json and browserrouter props

---
+++ precache-manifest.de2c4530080d1f5d073dd8dc342736a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468e5e29ef2994a5e5ef81017fd0cd01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3246eef477ac88415513",
+    "url": "/static/css/main.817a9343.chunk.css"
+  },
+  {
+    "revision": "80049be7be16d913873a",
+    "url": "/static/js/2.cfbc99b2.chunk.js"
+  },
+  {
+    "revision": "8ded5c4bdf4c7de2eb4864a500eacf42",
+    "url": "/static/js/2.cfbc99b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3246eef477ac88415513",
+    "url": "/static/js/main.3ec8d294.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3ec8d294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cba36deef3a7d2b858a",
+    "url": "/static/js/runtime-main.00c5e145.js"
+  },
+  {
+    "revision": "458142f4882510a687dcb5586ed4bd94",
+    "url": "/static/media/logo.458142f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bba76d7a37c9264e2990ae94fd5555e5.js"
+  "/precache-manifest.de2c4530080d1f5d073dd8dc342736a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
