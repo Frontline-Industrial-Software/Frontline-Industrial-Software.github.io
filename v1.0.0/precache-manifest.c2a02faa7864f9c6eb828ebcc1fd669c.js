c4eb4db5840cdd6d00268fc6f8e92e4162daab11
Ricky Ding <416640656@qq.com>
2023-03-01 20:27:33 +0800

    。。

---
+++ v1.0.0/precache-manifest.c2a02faa7864f9c6eb828ebcc1fd669c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5306e7ee884c2e81d4b6de66d0ce8c2e",
+    "url": "/v1.0.0/index.html"
+  },
+  {
+    "revision": "7d6e87b6dbf3ffa25d68",
+    "url": "/v1.0.0/static/css/2.fe38fc46.chunk.css"
+  },
+  {
+    "revision": "a39b9fca5458b063d1c1",
+    "url": "/v1.0.0/static/css/main.caeb0be9.chunk.css"
+  },
+  {
+    "revision": "7d6e87b6dbf3ffa25d68",
+    "url": "/v1.0.0/static/js/2.24e3b00d.chunk.js"
+  },
+  {
+    "revision": "72b4ff4e76c248c9d80111718d834d97",
+    "url": "/v1.0.0/static/js/2.24e3b00d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39b9fca5458b063d1c1",
+    "url": "/v1.0.0/static/js/main.b06bc914.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/v1.0.0/static/js/main.b06bc914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e13b50a51647fdc41e",
+    "url": "/v1.0.0/static/js/runtime-main.5e21b381.js"
+  }
+]);
--- v1.0.0/service-worker.js
+++ v1.0.0/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0104a6a07e2bba6980e9ee3fbcb2c8c.js"
+  "/v1.0.0/precache-manifest.c2a02faa7864f9c6eb828ebcc1fd669c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/v1.0.0/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
